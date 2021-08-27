912957a958a71b17d32fd06b659087ea628ce58e
Administrator <Administrator@DESKTOP-8F6DJTS>
2021-08-27 13:28:02 +0700

    Student

---
+++ Student/Student/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetInfo(string ten, string tuoi, string lop)
+        {
+            ViewBag.ten = ten;
+            ViewBag.tuoi = tuoi;
+            ViewBag.lop = lop;
+            return View();
+        }
+    }
+}
